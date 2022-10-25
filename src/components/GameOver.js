9a6fe3448c0f8dcea8efe31da41f2bcbd7af8ca3
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2022-10-25 15:33:36 -0400

    add basic modal css

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../App'
 import CreateGameConfirmation from './CreateGameConfirmation'
-function  GameOver({newCustomWordleTextInputRef, notify}) {
+import '../App.css';
+function GameOver({ newCustomWordleTextInputRef, notify }) {
 
-    const {gameOver, setGameOver, currAttempt, correctWord} = useContext(AppContext)
+  const { gameOver, setGameOver, currAttempt, correctWord } = useContext(AppContext)
 
   return (
-    <div className='gameOver'> 
-        <h3>{gameOver.guessedWord ? "You Correctly Guessed!" : "You Failed!"} </h3> 
+    <div className='gameOver'>
+      <div className="gameOver__content">
+        <h3>{gameOver.guessedWord ? "You Correctly Guessed!" : "You Failed!"} </h3>
         <h1> Correct Word: {correctWord}</h1>
         {gameOver.guessedWord && (<h3>You Guessed in {currAttempt.attempt} attempts</h3>)}
-        <CreateGameConfirmation newCustomWordleTextInputRef={newCustomWordleTextInputRef} notify={notify}/>
-     </div>
+        <CreateGameConfirmation newCustomWordleTextInputRef={newCustomWordleTextInputRef} notify={notify} />
+      </div>
+    </div>
   )
 }
 
