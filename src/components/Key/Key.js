d19439745fac8ffeb8a11157cfcd0207c24b6955
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2023-02-28 11:15:46 -0500

    fix conflict and remove console.logs

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -3,7 +3,9 @@ import { AppContext } from "../../App"
 import './Key.scss'
 import backspaceIcon from '../../assets/backspace_FILL0_wght400_GRAD0_opsz48.svg'
 function Key({ keyVal, bigKey, disabled }) {
-    const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+    const { onSelectLetter, onDelete, onEnter, commitedLetters} = useContext(AppContext);
+
+
 
     const selectLetter = () => {
         if (keyVal === "ENTER") {
@@ -15,9 +17,17 @@ function Key({ keyVal, bigKey, disabled }) {
         }
 
     }
+<<<<<<< HEAD
     
+=======
+
+    let keyStatus = ''
+    
+
+    // console.log(bigKey)
+>>>>>>> 1a30bbb (add logic and styling for keys)
     return (
-        <div className='key' id={bigKey ? "big" : disabled ?  "disabled" : ""} onClick={selectLetter}>
+        <div className={`key ${commitedLetters[keyVal] ? commitedLetters[keyVal] : '' }`} id={bigKey ? "big" : disabled ?  "disabled" : ""} onClick={selectLetter}>
             {keyVal === "DELETE"
                 ? <img className='key__backspace-icon' src={backspaceIcon} alt="Backspace Icon" />
                 : keyVal
